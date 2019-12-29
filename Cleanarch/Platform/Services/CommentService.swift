e3c53bb89eb750d891256c46503c300f9a6c1313
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2019-12-29 19:22:46 +0700

    Create Comment Service Implementation

--- Cleanarch/Platform/Services/CommentService.swift
+++ Cleanarch/Platform/Services/CommentService.swift
@@ -11,3 +11,29 @@ import Foundation
 protocol CommentService {
     func fetchComments(postId: Int, completion: ((Result<[Comment], Error>) -> Void))
 }
+
+class CommentServiceImpl: CommentService {
+    
+    init() { }
+    
+    func fetchComments(postId: Int, completion: ((Result<[Comment], Error>) -> Void)) {
+        func fetchPosts(completion: @escaping ((Result<[Post], Error>) -> Void)) {
+            let urlString = "https://jsonplaceholder.typicode.com/comments?postId=\(postId)"
+            let url = URL(string: urlString)
+            URLSession.shared.dataTask(with: url!) { (data, urlResponse, error) in
+                if let error = error {
+                    completion(.failure(error))
+                    return
+                }
+                guard let data = data else { return }
+                
+                do {
+                    let posts = try JSON().newJSONDecoder().decode([Post].self, from: data)
+                    completion(.success(posts))
+                } catch(let error) {
+                    completion(.failure(error))
+                }
+            }.resume()
+        }
+    }
+}
