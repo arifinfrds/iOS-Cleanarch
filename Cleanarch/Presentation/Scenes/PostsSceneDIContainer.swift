45f764c191b4b487b65624eeaf2b034014f5ed1e
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2020-02-26 22:12:42 +0700

    Enhance PostsSceneDIContainer for PostsViewController view

--- Cleanarch/Presentation/Scenes/PostsSceneDIContainer.swift
+++ Cleanarch/Presentation/Scenes/PostsSceneDIContainer.swift
@@ -9,6 +9,38 @@
 import UIKit
 
 final class PostsSceneDIContainer {
-
+    
+    struct Dependencies {
+        let postService: PostService
+    }
+    
+    private let dependencies: Dependencies
+    
+    init(dependencies: Dependencies) {
+        self.dependencies = dependencies
+    }
+    
+    
+    // MARK: - Posts List
+    func makePostsViewController() -> UIViewController {
+        let viewController = PostsViewController.create(with: makePostsViewModel())
+        return viewController
+    }
+    
+    func makePostsViewModel() -> PostsViewModel {
+        let viewModel: PostsViewModel = DefaultPostsViewModel(useCase: makeViewPostsUseCase())
+        return viewModel
+    }
+    
+    func makeViewPostsUseCase() -> ViewPostsUseCase {
+        let useCase: ViewPostsUseCase = ViewPostsUseCaseImpl(repository: makePostRepository())
+        return useCase
+    }
+    
+    func makePostRepository() -> PostRepository {
+        let repository: PostRepository = PostRepositoryImpl(postService: dependencies.postService)
+        return repository
+    }
+    
     
 }
