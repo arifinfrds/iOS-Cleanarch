fe492bbbe4a94843ab2c88992f4d05550f76a5af
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2020-11-14 17:15:29 +0700

    does not request data from url upon creation

--- CleanarchTests/CleanarchTests.swift
+++ CleanarchTests/CleanarchTests.swift
@@ -9,26 +9,29 @@
 import XCTest
 @testable import Cleanarch
 
+protocol HTTPClient { }
+
+class DefaultPostService {
+    private let client: HTTPClient
+    
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
+}
+
+
 class CleanarchTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    func test_init_doesNotRequestDataFromURL() {
+        let client = HTTPClientSpy()
+        let _ = DefaultPostService(client: client)
+        
+        XCTAssertEqual(client.requestedURLs, [])
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURLs: [URL] = []
     }
 
 }
