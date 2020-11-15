32b821b8fca3b3369085a8938767d499fac685ab
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2020-11-15 14:51:02 +0700

    guaranatee that can map items

--- CleanarchTests/CommentsMapperTests.swift
+++ CleanarchTests/CommentsMapperTests.swift
@@ -34,7 +34,7 @@ class CommentsMapperTests: XCTestCase {
         }
     }
     
-    func test_Map_deliversEmptyItems() {
+    func test_map_deliversEmptyItems() {
         let validJSONData = "[]".data(using: .utf8)!
         let completionResult = CommentsMapper.map(validJSONData)
         
@@ -45,5 +45,46 @@ class CommentsMapperTests: XCTestCase {
             XCTFail("expect success, but got failure instead with error: \(error)")
         }
     }
-
+    
+    func test_map_deliversItems() {
+        let validJSONData = loadJSONData(forResource: "comments-response", extensionName: "json")
+        let completionResult = CommentsMapper.map(validJSONData)
+        
+        switch completionResult {
+        case .success(let comments):
+            XCTAssertEqual(comments, makeExpectedComments())
+        case .failure(let error):
+            XCTFail("expect success, but got failure instead with error: \(error)")
+        }
+    }
+    
+    
+    // MARK: - Helpers
+    
+    private func loadJSONData(forResource resource: String, extensionName: String) -> Data {
+        let bundle = Bundle(for: CommentsMapperTests.self)
+        let resourceURL = bundle.url(forResource: resource, withExtension: extensionName)!
+        let data = try! Data(contentsOf: resourceURL)
+        return data
+    }
+    
+    private func makeExpectedComments() -> [Comment] {
+        return [
+            Comment(
+                postID: 1,
+                id: 1,
+                name: "id labore ex et quam laborum",
+                email: "Eliseo@gardner.biz",
+                body: "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
+            ),
+            Comment(
+                postID: 1,
+                id: 2,
+                name: "quo vero reiciendis velit similique earum",
+                email: "Jayne_Kuhic@sydney.com",
+                body: "est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et"
+            )
+        ]
+    }
+    
 }
